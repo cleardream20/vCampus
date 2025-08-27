b8ab21eb884079111f756649d9566759910733d7
Tmilk123 <1185326979@qq.com>
2025-08-27 19:50:28 +0800

    test一下

--- vCampus-server/src/main/java/com/seu/vcampus/server/ServerMain.java
+++ vCampus-server/src/main/java/com/seu/vcampus/server/ServerMain.java
@@ -5,7 +5,7 @@ import com.seu.vcampus.common.model.User;
 public class ServerMain {
     public static void main(String[] args) {
         User user = new User(
-                "123456789",
+                "123456789123",
                 "seu",
                 "123@seu.edu.cn",
                 "123456",

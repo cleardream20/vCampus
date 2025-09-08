7b3dc3f2cddab1c2d82b5a7360448a2ac457e6ed
cleardream20 <2668526118@qq.com>
2025-09-08 17:43:33 +0800

    server's Login test

--- vCampus-server/src/main/java/com/seu/vcampus/server/ServerMain.java
+++ vCampus-server/src/main/java/com/seu/vcampus/server/ServerMain.java
@@ -1,12 +1,41 @@
 package com.seu.vcampus.server;
 
-import com.seu.vcampus.common.model.User;
 import com.seu.vcampus.server.socket.ServerSocketThread;
+import javax.swing.*;
 
+/**
+ * 服务器主入口
+ * 启动 Socket 服务，监听客户端连接
+ */
 public class ServerMain {
+
     public static void main(String[] args) {
-        ServerSocketThread server = new ServerSocketThread();
-        server.start();
-        System.out.println("图书馆服务端已启动...");
+        // 可选：启动一个简单的 GUI 显示服务器状态（非必须）
+        showServerStatusWindow();
+
+        // 启动 Socket 服务线程
+        ServerSocketThread serverThread = new ServerSocketThread(8888);
+        serverThread.start();
+
+        System.out.println("vCampus 服务器已启动，监听端口 8888");
+        System.out.println("等待客户端连接...");
+    }
+
+    /**
+     * 可选：显示一个简单的服务器状态窗口
+     */
+    private static void showServerStatusWindow() {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("vCampus Server");
+            frame.setSize(300, 150);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setLocationRelativeTo(null);
+
+            JLabel label = new JLabel("服务器运行中...", SwingConstants.CENTER);
+            label.setFont(label.getFont().deriveFont(16f));
+
+            frame.add(label);
+            frame.setVisible(true);
+        });
     }
 }
--- vCampus-server/src/main/java/com/seu/vcampus/server/controller/UserController.java
+++ vCampus-server/src/main/java/com/seu/vcampus/server/controller/UserController.java
@@ -1,112 +1,37 @@
 package com.seu.vcampus.server.controller;
 
-import com.google.gson.Gson;
 import com.seu.vcampus.common.model.User;
+import com.seu.vcampus.common.util.Jsonable;
 import com.seu.vcampus.common.util.Message;
+import com.seu.vcampus.server.service.UserService;
 import com.seu.vcampus.server.service.UserServiceImpl;
-import lombok.Data;
 
 import java.sql.SQLException;
 
 public class UserController {
-    private final UserServiceImpl userService = new UserServiceImpl();
-    private final Gson gson = new Gson();
 
-    @Data
-    private static class LoginRequest {
-        private String cid;
-        private String password;
-    }
+    private final UserService userService = new UserServiceImpl();
 
-    @Data
-    private static class RegisterRequest {
-        private String cid;
-        private String password;
-        private String tsid;
-        private String confirmPassword;
-        private String email;
-        private String phone;
-        private String name;
-        private String role;
-    }
+    public Message handleRequest(Message request) throws SQLException {
+        String type = request.getType();
 
-    public String handleMessage(String requestJson) {
-        try {
-            Message request = Message.fromJson(requestJson);
-
-            return switch (request.getType()) {
-                case Message.TYPE_LOGIN -> handleLoginRequest(request);
-                case Message.TYPE_REGISTER -> handleRegisterRequest(request);
-                default -> createErrorResponse("暂未支持: " + request.getType());
-            };
-        } catch (Exception e) {
-            return createErrorResponse("服务端请求处理异常: " + e.getMessage());
+        switch (type) {
+            case Message.TYPE_LOGIN:
+                User user = Jsonable.fromJson(request.getData(), User.class);
+                if (user == null) {
+                    return Message.fromData(type, "no", false, "用户数据格式错误");
+                }
+                User result = userService.Login(user.getCid(), user.getPassword());
+                if (result != null) {
+                    return Message.fromData(Message.TYPE_LOGIN, result.toJson(), true, "登录成功");
+                } else {
+                    return Message.fromData(type, "no", false, "账号或密码错误");
+                }
+            case Message.TYPE_REGISTER:
+                // userService.register(...)
+                return Message.fromData(type, "no", false, "注册功能开发中");
+            default:
+                return Message.fromData(Message.TYPE_RESPONSE, "no", false, "不支持的操作");
         }
-    }
-
-    private String handleLoginRequest(Message request) {
-        try {
-            LoginRequest loginRequest = gson.fromJson(request.getData(), LoginRequest.class);
-            String cid = loginRequest.getCid();
-            String password = loginRequest.getPassword();
-
-            if(cid == null || password == null) {
-                return createErrorResponse("一卡通号或密码不能为空！");
-            }
-
-            User user = userService.Login(cid, password);
-            if(user != null) {
-                Message response = new Message();
-                response.setType(Message.TYPE_RESPONSE);
-                response.setSuccess(true);
-                response.setMessage("登陆成功");
-                response.setData(gson.toJson(user));
-                return response.toJson();
-            } else {
-                return createErrorResponse("一卡通号或密码错误");
-            }
-
-        } catch (SQLException e) {
-            return createErrorResponse("数据库错误: " + e.getMessage());
-        }
-    }
-
-    private String handleRegisterRequest(Message request) {
-        try {
-            RegisterRequest  registerRequest = gson.fromJson(request.getData(), RegisterRequest.class);
-            String cid = registerRequest.getCid();
-            String password = registerRequest.getPassword();
-            String tsid = registerRequest.getTsid();
-            String confirmPassword = registerRequest.getConfirmPassword();
-            String email = registerRequest.getEmail();
-            String phone = registerRequest.getPhone();
-            String name = registerRequest.getName();
-            String role = registerRequest.getRole();
-            if(cid == null || password == null || confirmPassword == null || email == null || phone == null || name == null) {
-                return createErrorResponse("请填写完整信息");
-            } else if(password.length() < 6) {
-                return createErrorResponse("密码长度不能小于6");
-            } else if(!password.equals(confirmPassword)) {
-                return createErrorResponse("前后两次密码不一样，请检查");
-            } else {
-                User user = new User(cid, password, tsid, name, email, phone, role);
-                Message response = new Message();
-                response.setType(Message.TYPE_RESPONSE);
-                response.setSuccess(true);
-                response.setMessage("注册成功");
-                response.setData(gson.toJson(user));
-                return response.toJson();
-            }
-        } catch (Exception e) {
-            return createErrorResponse("数据库错误: " + e.getMessage());
-        }
-    }
-
-    private String createErrorResponse(String errorMessage) {
-        Message response = new Message();
-        response.setType(Message.TYPE_RESPONSE);
-        response.setSuccess(false);
-        response.setMessage(errorMessage);
-        return response.toJson();
     }
 }

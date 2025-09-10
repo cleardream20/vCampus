ab0a57f14bc4001591a3884e9f527b73c0d5deb4
cleardream20 <2668526118@qq.com>
2025-09-10 10:59:02 +0800

    merging...

--- vCampus-common/src/main/java/com/seu/vcampus/common/util/Message.java
+++ vCampus-common/src/main/java/com/seu/vcampus/common/util/Message.java
@@ -1,7 +1,5 @@
 package com.seu.vcampus.common.util;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,60 +7,32 @@ import lombok.NoArgsConstructor;
 import java.io.Serializable;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 public class Message implements Serializable {
-//    public static final String STATUS_SUCCESS = "SUCCESS";
-//    public static final String STATUS_ERROR = "ERROR";
+    private static final long serialVersionUID = 1L;
+    public static final String LOGIN = "LOGIN";
+    public static final String REGISTER = "REGISTER";
+    public static final String LOGOUT = "LOGOUT";
+    public static final String ENTER_USER_CENTER = "ENTER_USER_CENTER";
+    public static final String CONTROL_STUDENT =  "CONTROL_STUDENT";
+    public static final String SELECT_COURSE = "SELECT_COURSE";
 
-    public static final String TYPE_LOGIN = "LOGIN";
-    public static final String TYPE_REGISTER = "REGISTER";
-    public static final String TYPE_RESPONSE = "RESPONSE";
+    public static final String STATUS_SUCCESS = "SUCCESS";
+    public static final String STATUS_ERROR = "ERROR";
 
     private String type;
-//    private String status;
-    private String data; // String instead of Obj, less "toString()" needed
-                         // So toString() method is necessary
+    private String status;
+    private Object data;
+    private String message; // 帮助信息
 
-    private boolean success; // 是否成功（用于响应）
-    private String message; // 提示信息
-
-    //很标准，AI就行了
-
-    public Message(String type, String data) {
-        this.type = type;
-        this.data = data;
-    }
-
-    // 工厂方法
-    // e.g. return Message.success(Message.TYPE_LOGIN, user);
     public static Message success(String type, Object data) {
-        return new Message(type, Jsonable.toJson(data), true, "OK");
+//        return new Message(type, STATUS_SUCCESS, data);
+        return new Message(type, STATUS_SUCCESS, Jsonable.toJson(data), "OK");
     }
 
-    public static Message error(String type, String errorMsg) {
-        return new Message(type, null, false, errorMsg);
+    public static Message error(String type, String error) {
+        return new Message(type, STATUS_ERROR, null, error);
     }
 
-    public static Message of(String type, Object data) {
-        return new Message(type, Jsonable.toJson(data), true, "OK");
-    }
-
-    public String toJson() {
-        Gson gson = new GsonBuilder().create();
-        return gson.toJson(this);
-    }
-
-    public static Message fromJson(String json) {
-        Gson gson = new GsonBuilder().create();
-        return gson.fromJson(json, Message.class);
-    }
-
-    public static Message fromData(String type, String data, boolean success, String message) {
-        return new Message(type, data, success, message);
-    }
-
-    public boolean isSuccess() {
-        return success;
-    }
 }

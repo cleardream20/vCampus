8b3217107985898ded24a6b5c4f81b8ccefb7497
aimyon <1994664200@qq.com>
2025-09-13 12:58:11 +0800

    删除controller,等待添加service

--- vCampus-client/src/main/java/com/seu/vcampus/client/controller/Student/STController.java
+++
@@ -1,9 +0,0 @@
-package com.seu.vcampus.client.controller.Student;
-
-import com.seu.vcampus.common.model.Student;
-
-public class STController {
-    public Student getStudent(String cid) {
-        return new Student();
-    }
-}
--- vCampus-client/src/main/java/com/seu/vcampus/client/controller/student/STController.java
+++
@@ -1,9 +0,0 @@
-package com.seu.vcampus.client.controller.student;
-
-import com.seu.vcampus.common.model.Student;
-
-public class STController {
-    public Student getStudent(String cid) {
-        return new Student();
-    }
-}
